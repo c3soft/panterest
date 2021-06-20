226540f2d4bb3b43d97ca1296536cd40b5b16444
Pascal <it.tech@laposte.net>
2021-06-20 09:50:20 +0200

    first commit

--- migrations/Version20210620071621.php
+++ migrations/Version20210620071621.php
@@ -10,17 +10,17 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210130095512 extends AbstractMigration
+final class Version20210620071621 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Création de la table pins';
+        return '';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE pins (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pins (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description CLOB NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)');
     }
 
     public function down(Schema $schema) : void
